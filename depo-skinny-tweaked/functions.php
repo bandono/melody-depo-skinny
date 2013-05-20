eff824e6b3ca0c5956af19d062d867ed05dba7ff
Arif Kusbandono <arif.working@gmail.com>
2013-05-20 19:36:24 +0700

    v2.2 major comment revamp add depotwk_comment in functions.php

--- depo-skinny-tweaked/functions.php
+++ depo-skinny-tweaked/functions.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package WordPress
- * @subpackage DePo Skinny Tweaked
+ * @subpackage DePo_Skinny_Tweaked
  */
 
 /**
@@ -30,6 +30,8 @@ function depotwk_setup() {
 
 /**
  * Admin Theme Page.
+ * 
+ * @since Depo Skinny Tweaked 2.2
  */
 add_action( 'admin_menu', 'depotwk_add_theme_page' );
 
@@ -72,13 +74,78 @@ function depotwk_theme_page() {
 	</div>
 <?php }
 
+if ( ! function_exists( 'depotwk_comment' ) ) :
+/**
+ * Template for comments and pingbacks.
+ *
+ * Modified from twentytwelve_comment() in Twenty Twelve 1.1 theme
+ *
+ * Used as a callback by wp_list_comments() for displaying the comments.
+ *
+ * @since Depo Skinny Tweaked 2.2
+ */
+function depotwk_comment( $comment, $args, $depth ) {
+	$GLOBALS['comment'] = $comment;
+	switch ( $comment->comment_type ) :
+		case 'pingback' :
+		case 'trackback' :
+		// Display trackbacks differently than normal comments.
+	?>
+	<li <?php comment_class(); ?> id="comment-<?php comment_ID(); ?>">
+		<p><?php _e( 'Pingback:', 'depo-skinny-tweaked' ); ?> <?php comment_author_link(); ?> <?php edit_comment_link( __( '(Edit)', 'depo-skinny-tweaked' ), '<span class="edit-link">', '</span>' ); ?></p>
+	<?php
+			break;
+		default :
+		// Proceed with normal comments.
+		global $post;
+	?>
+	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
+		<article id="comment-<?php comment_ID(); ?>" class="comment">
+			<header class="comment-meta comment-author vcard">
+				<?php
+					echo get_avatar( $comment, 44 );
+					printf( '<cite class="fn">%1$s %2$s</cite>',
+						get_comment_author_link(),
+						// If current post author is also comment author, make it known visually.
+						( $comment->user_id === $post->post_author ) ? '<span> ' . __( 'Post author', 'depo-skinny-tweaked' ) . '</span>' : ''
+					);
+					printf( '<a href="%1$s"><time datetime="%2$s">%3$s</time></a>',
+						esc_url( get_comment_link( $comment->comment_ID ) ),
+						get_comment_time( 'c' ),
+						/* translators: 1: date, 2: time */
+						sprintf( __( '%1$s at %2$s', 'depo-skinny-tweaked' ), get_comment_date(), get_comment_time() )
+					);
+				?>
+			</header><!-- .comment-meta -->
+
+			<?php if ( '0' == $comment->comment_approved ) : ?>
+				<p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'depo-skinny-tweaked' ); ?></p>
+			<?php endif; ?>
+
+			<section class="comment-content comment">
+				<?php comment_text(); ?>
+				<?php edit_comment_link( __( 'Edit', 'depo-skinny-tweaked' ), '<p class="edit-link">', '</p>' ); ?>
+			</section><!-- .comment-content -->
+
+			<div class="reply">
+				<?php comment_reply_link( array_merge( $args, array( 'reply_text' => __( 'Reply', 'depo-skinny-tweaked' ), 'after' => ' <span>&darr;</span>', 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+			</div><!-- .reply -->
+		</article><!-- #comment-## -->
+	<?php
+		break;
+	endswitch; // end comment_type check
+}
+endif;
+
 /**
  * Additional options for wp-stats-view-counter integration
  * to retrieve Jetpack stats and update locally every 1 hour
+ * 
+ * @since Depo Skinny Tweaked 2.2
  */
-add_filter( 'view_counter_transient_expiration', 'my_view_counter_transient_expiration', 10, 1 );
+add_filter( 'view_counter_transient_expiration', 'depotwk_view_counter_transient_expiration', 10, 1 );
 
-function my_view_counter_transient_expiration( $hours ) {
+function depotwk_view_counter_transient_expiration( $hours ) {
 
     return 1; // time in hours
 
